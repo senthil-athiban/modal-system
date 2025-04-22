f8eb9d70934f78cb7b047c2e819500dfb806fb8b
senthil-athiban <senthilathiban0@gmail.com>
2025-04-22 17:58:52 +0530

    (refactor) remove_unused_files

--- src/modal.container.tsx
+++ src/modal.container.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { modalStateStore, useModalManager, ModalInstance, ModalProviderProps } from '.';
 import { createPortal } from 'react-dom';
 
