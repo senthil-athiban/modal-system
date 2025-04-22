fdfc68bc7c0429c8376a0130fa172f6499eda00a
senthil-athiban <senthilathiban0@gmail.com>
2025-04-22 19:47:41 +0530

    (fix) export_type

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export type * from './types/modal';
 export * from './store/modal.store';
 export * from './hooks';
 export { default as ModalProvider } from './components/modal-provider.component';
-export { ModalError } from './error-handler';
+export type { ModalError } from './error-handler';
